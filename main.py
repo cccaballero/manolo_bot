bfc0a30467ae7e44d55c4ebc38a28dc6af7f8be7
Carlos Cesar Caballero <ccesar@daxslab.com>
2023-04-06 20:35:33 -0400

    add Markdown formatting

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ def echo_all(message):
             replace = f'@{bot_username}: '
             if response_content.startswith(replace):
                 response_content = response_content[len(replace):]
-            bot.reply_to(message, response_content)
+            bot.reply_to(message, response_content, parse_mode='markdown')
         chats[chat_id]['messages'].append({"role": "assistant", "content": response_content})
 
 
